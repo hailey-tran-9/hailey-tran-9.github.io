c371455e87595ef99dbd28124b1d49883a76021a
Hailey Tran <101952089+hailey-tran-9@users.noreply.github.com>
2023-08-30 13:46:31 -0700

    Add game start overlay

---
+++ public/script.js
@@ -0,0 +1,9 @@
+gameStartOverlayOpen = !$( "#gameStartOverlay" )[0].hidden;
+
+// Handle document clicks
+$( document ).on( "click", function() {
+    if (gameStartOverlayOpen) {
+        $( "#gameStartOverlay" ).hide();
+        gameStartOverlayOpen = false;
+    }
+} );
