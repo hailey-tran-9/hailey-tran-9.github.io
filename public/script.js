3c957f3fa774a4abf80df523ff6757971dfe2a6d
Hailey Tran <101952089+hailey-tran-9@users.noreply.github.com>
2023-08-31 19:31:35 -0700

    Open and close stat block

--- public/script.js
+++ public/script.js
@@ -8,3 +8,24 @@ $( document ).on( "click", function() {
         gameStartOverlayOpen = false;
     }
 } );
+
+// Handle keyboard shortcuts
+$( document ).on( "keydown", function( e ) {
+    console.log(e.key);
+    if (e.key == "p") {
+        ToggleCharProf();
+    }
+} );
+
+charStat = $( "#charStatBlock" )[0];
+charStatOpen = false;
+
+function ToggleCharProf() {
+    if (charStatOpen) {
+        charStat.style.visibility = "hidden";
+        charStatOpen = false;
+    } else {
+        charStat.style.visibility = "visible";
+        charStatOpen = true;
+    }
+}
