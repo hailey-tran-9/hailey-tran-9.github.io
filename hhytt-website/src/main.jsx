3532b2f32b3dbc8767759e826adf842bc3292fea
hailey-tran-9 <101952089+hailey-tran-9@users.noreply.github.com>
2023-09-10 22:32:54 -0700

    Add 3 project info

--- hhytt-website/src/main.jsx
+++ hhytt-website/src/main.jsx
@@ -7,24 +7,45 @@ import $ from 'jquery';
 
 import { createRoot } from 'react-dom/client';
 
-function CreateProject(mt, gt) {
+function CreateProject(mt, gt, link, roleTime, description, tasks) {
     return (
-        <div className="row" style={{"marginTop": mt}}>
-            <div className="col-4 d-flex justify-content-center">
-              <img src="/imgs/[ME][TA]L.png" width="155px" 
-                height="150px" style={{"objectFit": "contain", "marginLeft": "7px"}}></img>
+        <>
+            <div className="row" style={{"marginTop": mt}}>
+                <div className="col-4 d-flex justify-content-center">
+                <img src={"/imgs/"+gt.replace(/\s+/g, '')+".png"} width="155px" 
+                    height="155px" style={{"objectFit": "cover", "marginLeft": "7px"}}></img>
+                </div>
+                <div className="col-8" style={{"paddingLeft": "1.75rem"}}>
+                    <div className="row"><h3 className="pr-3 pt-1 mb-0">{gt}</h3></div>
+                    <div className="row"><a href={link} target="_blank">Play the game here!</a></div>
+                    <div className="row mt-5"><p className="pt-2">{roleTime}</p></div>
+                </div>
             </div>
-            <div className="col-8">
-              <h3 className="pl-1 pr-3">{gt}</h3>
+            <div className="row" style={{"marginTop": "3.5rem", "paddingLeft": "45px", "paddingRight": "155px", "whiteSpace": "pre-wrap"}}>
+                <p>{description}</p>
+                <ul style={{"paddingLeft": "2rem"}}>
+                    {tasks.map(task => (
+                        <li key={task}>{task}</li>
+                    ))}
+                </ul>
             </div>
-        </div>
+        </>
+        
     )
 }
 
 const projectsRoot = createRoot(document.getElementById( "projectsContainer" ));
-projectsRoot.render(CreateProject("50px", "[ME][TA]L"));
+projectsRoot.render(CreateProject("45px", "[ME][TA]L", "https://penguinies.itch.io/metal", "Programmer | 2 months",
+    "[ME][TA]L is a metal-themed rhythm game. Its unique feature is the burst note, which requires you to type the words on the screen in a limited time frame!",
+    ["Implemented UI functionality", "Aligned note recordings to our parser's structure", "Created the tutorial"]));
 
+projectsRoot.render(CreateProject("45px", "LemmeDoIt4U", "https://zenuriken.itch.io/lemmedoit4u", "Programmer | 2 days",
+    "LemmeDoIt4U is a short game about a dog sticking its nose into a can to get the last pringle chip for its owner while dodging alien chips! The theme of the game jam was 'Into the Unknown.'",
+    ["Implemented UI and meme popup functionality", "Set up audio and buff timers"]));
 
+projectsRoot.render(CreateProject("45px", "Morpheus' (Unpaid) Intern", "https://weest.itch.io/morpheus-intern-unpaid", "Programmer | 2 days",
+    "Morpheus' (Unpaid) Intern is a bullet-hell, where you play as the new intern of the god of dreams. Protect the sleeping child from nightmares!",
+    ["Implemented UI functionality", "Created buff/debuff spawner and effects"]));
 
 var progress = 0;
 var gameStartOverlayOpen = !$( "#gameStartOverlay" )[0].hidden;
